27a9db380ea9a009a50e5a62d150a2969c08ebcc
Noah Weiner <noah.weiner@yale.edu>
2021-01-03 01:53:59 -0500

    reroute root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  #add route that maps root path of our app to appropriate controller and action
+  #root route is mapped to index action of articles controller
+  root "articles#index"
+
+
   get "/articles", to: "articles#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
