4fb3a361a9107b3542d1f5106d2176f1152f768a
Noah Weiner <noah.weiner@yale.edu>
2021-01-04 00:25:42 -0500

    display test articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
   def index
+    #controller instance vars can be accessed by the view, so @articles can be seen by app/views/articles/index.html.erb
+    @articles = Article.all
   end
 end
