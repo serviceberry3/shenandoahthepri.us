2dcff9ced0241183778085190074c0a177a0f4b2
Noah Weiner <noah.weiner@yale.edu>
2021-01-04 00:49:58 -0500

    add linking from titles to articles pgs

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,13 @@ class ArticlesController < ApplicationController
     #controller instance vars can be accessed by the view, so @articles can be seen by app/views/articles/index.html.erb
     @articles = Article.all
   end
+
+
+  def show
+    #find specific article from id captured by route parameter
+    #Returned art is stored in @article instance var, accessible by the view By default, the show action will render app/views/articles/show.html.erb
+    @article = Article.find(params[:id])
+  end
+
+
 end
