5808cec52608ad3d7d15bf7582450a964f930daa
Noah Weiner <noah.weiner@yale.edu>
2021-01-13 07:21:40 +0000

    db schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_01_03_065556) do
 
-  create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "articles", charset: "latin1", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
