e7b686ec068e452f2af9ce183c63fd665b059954
Noah Weiner <noah.weiner@yale.edu>
2021-01-14 01:36:27 -0500

    add commenting

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,14 @@
 #NOTE: Active Record automatically defines model attributes for every table col, so don't have to declare them in model file
-# @title is an instance var, available to all methods within the class (and available to the view), belongs to that one instance of the class
+# @title is an instance var, available to all methods within the class (and available to the view and all partials rendered in the view), 
+#belongs to that one instance of the class
 # title is a local var, exists only within its scope (current block/method)
 
 class Article < ApplicationRecord
+    #when you have has_many on one model linking to belongs_to on the other model, some nice behaviors come stock
+    #e.g. if you have instance var @article containing an article, you can retrieve all comments belonging to that art as array using @article.comments
+    has_many :comments
+
+
     #if any validation fails, save will be aborted, appropriate error mesgs will be added to errors attribute of the model object
 
 
