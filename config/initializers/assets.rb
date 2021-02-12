fc8e186d33d71ef0351d9a4fe086109e77df1afd
Noah Weiner <noah.weiner@yale.edu>
2021-02-12 06:50:56 +0000

    add map, gpx

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,3 +14,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'javascripts')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'gpx')
+
